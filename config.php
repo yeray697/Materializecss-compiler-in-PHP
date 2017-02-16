48df5bb2594cf945d356a7a8caa446c9c9d01a24
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-16 18:37:55 +0100

    Working!

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+DEFINE("MATCOMPILER_PATH",__DIR__);
+DEFINE("SCSSPHP_PATH",MATCOMPILER_PATH."/scssphp");
+DEFINE("MATERIALIZE_PATH",MATCOMPILER_PATH."/materializecss_sass");
+require_once(SCSSPHP_PATH."/scss.inc.php");
+?>
